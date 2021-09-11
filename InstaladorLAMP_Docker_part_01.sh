363f7d4d0ad08756672c06938ba62d72f93368e7
Rodrigo Bastias <rbastias@utem.cl>
2021-09-11 20:23:10 -0300

    otros cambios

--- InstaladorLAMP_Docker_part_01.sh
+++ InstaladorLAMP_Docker_part_01.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 sudo tasksel install lamp-server
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
-sudo add-apt-repository «deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable»
+sudo apt-get update
+ sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt install -y php-curl php-gd php-intl php-mbstring php-xml php-zip
 sudo apt install -y php-apcu php-memcached php-memcache
 sudo systemctl reload apache2

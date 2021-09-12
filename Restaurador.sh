aef017bab678fec7094bfe37f3f0a3269212fd4a
Rodrigo Bastias <rbastias@utem.cl>
2021-09-11 23:18:32 -0300

    dejando personalizacion como comentario

--- Restaurador.sh
+++ Restaurador.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Averigüe su nombre de usuario antes de usar este script, asi cambia "tu usuario" y descomenta la siguiente linea
+# 1.- algunos items que aparecen acá estan como comentario, puesto que son las cosas que ocupo o suelo ocupar, si le sirve descomentelas.
+# 2.- Averigüe su nombre de usuario antes de usar este script, asi cambia "tu usuario" y descomenta la siguiente linea
 #cd /home/'tu usuario'/
 sudo apt update; sudo apt upgrade -y; sudo snap refresh
 sudo apt install chromium-browser -y
@@ -16,9 +17,13 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-* -y
 rm google-chrome-stable_current_amd64.deb
-wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
-sudo apt install ./steam.deb -y
-rm steam.deb
+sudo apt install chrome-gnome-shell -y
+#
+# Steam
+#wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+#sudo apt install ./steam.deb -y
+#rm steam.deb
+#
 wget https://az764295.vo.msecnd.net/stable/e7d7e9a9348e6a8cc8c03f877d39cb72e5dfb1ff/code_1.60.0-1630494279_amd64.deb
 sudo apt install ./code_* -y
 rm code_1.60.0-1630494279_amd64.deb
@@ -42,16 +47,22 @@ sudo snap install spotify
 sudo snap install discord
 sudo snap connect discord:system-observe
 sudo snap install whatsdesk
-sudo snap install freecad
-wget https://updates.tdesktop.com/tlinux/tsetup.3.0.1.tar.xz
-tar -Jxvf tsetup.3.0.1.tar.xz
-rm tsetup.3.0.1.tar.xz
-sudo apt install nautilus-admin nautilus-nextcloud nautilus-dropbox -y  
-sudo apt install gnome-shell-extensions gnome-tweaks gnome-shell-extension-arc-menu chrome-gnome-shell -y
-sudo apt install plank -y
-sudo apt install obsidian-icon-theme -y
-sudo apt install numix-* -y
-sudo apt install papirus-* -y
+#
+#FreeCAD
+#sudo snap install freecad
+#
+#Telegram
+#wget https://updates.tdesktop.com/tlinux/tsetup.3.0.1.tar.xz
+#tar -Jxvf tsetup.3.0.1.tar.xz
+#rm tsetup.3.0.1.tar.xz
+#
+# Parte de mi personalizacion
+#sudo apt install nautilus-admin nautilus-nextcloud nautilus-dropbox -y  
+#sudo apt install gnome-shell-extensions gnome-tweaks gnome-shell-extension-arc-menu chrome-gnome-shell -y
+#sudo apt install plank -y
+#sudo apt install obsidian-icon-theme -y
+#sudo apt install numix-* -y
+#sudo apt install papirus-* -y
 notify-send " Listo, puede continuar con el Instalador...Part_01 "
 echo " Listo, puede continuar con el Instalador...Part_01 "
 exit 

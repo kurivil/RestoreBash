a65270f14e55235f08ca6d53f63fd74a678cd393
Rodrigo Bastias <rbastias@utem.cl>
2021-09-11 22:02:02 -0300

    este si es el ultimo

--- Instalador_Sin_Docker_Part_02.sh
+++ Instalador_Sin_Docker_Part_02.sh
@@ -9,9 +9,11 @@ wget https://download.prestashop.com/download/releases/prestashop_1.7.7.7.zip
 mkdir PrestaShop
 unzip prestashop_1.7.7.7.zip -d /var/www/html/PrestaShop
 rm prestashop_1.7.7.7.zip
+sudo chmod 777 PrestaShop
 wget https://cl.wordpress.org/latest-es_CL.zip
 unzip latest-es_CL.zip
 rm latest-es_CL.zip
+sudo chmod 777 wordpress
 ls
 cd
 notify-send " Listo, pero esto solo instaló los archivos en su carpeta respectiva. Para hacer un script de docker necesito un poco más de tiempo "

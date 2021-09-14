4e68f750ca42187be5c15edaaf46b11c2cc1c467
Rodrigo Bastias <rbastias@utem.cl>
2021-09-14 10:57:29 -0300

    Añadido en el bash "PhpMyAdmin", cambio de nombre del script

--- InstaladorLAMP_PhpMyAdmin_Docker_part_01.sh
+++ InstaladorLAMP_PhpMyAdmin_Docker_part_01.sh
@@ -20,6 +20,10 @@ sudo apt install -y php-apcu php-memcached php-memcache
 sudo systemctl reload apache2
 sudo a2enmod rewrite
 sudo systemctl restart apache2
+#
+#Esto instala PhpMyAdmin, recuerde que tiene que elegir Apache2
+#
+sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl
 notify-send " Por favor, lea el archivo "Leer aquí" y despues continue con Instalador...part_02 "
 echo " Por favor, lea el archivo "Leer aquí" y despues continue con Instalador...part_02 "
 exit

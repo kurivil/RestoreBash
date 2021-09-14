2b48773c8b411cc590e6422f38b307e9c92535be
Rodrigo Bastias <rbastias@utem.cl>
2021-09-14 17:10:42 -0300

    listo

--- InstaladorLAMP_PhpMyAdmin_Docker_part_01.sh
+++ InstaladorLAMP_PhpMyAdmin_Docker_part_01.sh
@@ -25,9 +25,8 @@ sudo systemctl restart apache2
 #
 #Esto instala PhpMyAdmin, recuerde que tiene que elegir Apache2
 #
-sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl
+sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 #
-#la clave por defecto queda guardada en /etc/mysql/debian.cnf
 #acceda mediante sudo nano /etc/mysql/debian.cnf
 #
 notify-send " Por favor, lea el archivo "Leer aquí" y despues continue con Instalador...part_02 "
